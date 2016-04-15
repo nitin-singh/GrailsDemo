d0583681892b7ba4cc459662de903e0a1a56e685
Nitin <nitinkoemail@gmail.com>
2016-04-15 20:20:22 +0530

    Minor changes

--- src/main/groovy/com/ttnd/demo/CO/SearchCO.groovy
+++ src/main/groovy/com/ttnd/demo/CO/SearchCO.groovy
@@ -1,12 +1,37 @@
 package com.ttnd.demo.CO
 
 import grails.validation.Validateable
+import grails.converters.JSON
+import org.grails.web.json.JSONArray
+import org.grails.web.json.JSONObject
 
 class SearchCO implements Validateable {
-    String sortColumn = "id"
+
+    Integer length
+    Integer start
+
     String order
-    String sort
-    String q
-    Integer max
-    Integer offset
+    String columns
+    String search
+
+
+    String fetchSearchValue(){
+        JSONObject js = JSON.parse(search)
+        return  js.get('value')
+    }
+
+    Integer fetchColumnIndex(){
+        JSONArray js = JSON.parse(order)
+        return  js.first().get("column")
+    }
+
+    String fetchSortingOrder(){
+        JSONArray js = JSON.parse(order)
+        return js.first().get("dir").trim()
+    }
+
+    String fetchSortProperty(){
+        JSONArray js = JSON.parse(columns)
+        return js.getJSONObject(fetchColumnIndex()).get("data").trim()
+    }
 }
